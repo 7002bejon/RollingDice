7d90c11cd1bcca6c95d475aaeaa72959419edd62
7002bejon <114094974+7002bejon@users.noreply.github.com>
2023-10-02 19:19:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ function rollDice() {
     const para = document.createElement("p");
     const para1 = document.createElement("p");
     const para2 = document.createElement("p");
-    const num1 = (Math.random() * 6) + 1;
-    const num2 = (Math.random() * 6) + 1;
-    para.textContent = num1;
-    para1.textContent = num2;
-    para2.textContent = num1 + num2;
+    num1 = parseInt((Math.random() * 6) + 1);
+    num2 = parseInt((Math.random() * 6) + 1);
+    num3 = num1 + num2
+    para.textContent = "Num 1 = " + num1;
+    para1.textContent = "Num 2 = " + num2;
+    para2.textContent = "Result = " + num3;
+    document.body.appendChild(para);
+    document.body.appendChild(para1);
+    document.body.appendChild(para2);
 }
 const button = document.querySelectorAll('button');
 for (const button of buttons) {
     button.addEventListener("click", rollDice);
 }
+
